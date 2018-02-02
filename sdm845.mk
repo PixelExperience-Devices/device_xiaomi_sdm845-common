9e440b39bcd7f9ed35bc5a7c019b0eaab0cdb3c1
Wei Wang <wvw@google.com>
2018-02-02 13:01:38 -0800

    sdm845-common: add SysUI to PRODUCT_DEXPREOPT_SPEED_APPS
    Test: Build
    Change-Id: I088938462b389e1ebe783997f773dc5946345666
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -142,6 +142,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
