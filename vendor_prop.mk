5decbccd39aad3a904290d8313c342a7ed187b8d
Suren Baghdasaryan <surenb@google.com>
2018-04-11 19:49:31 -0700

    sdm845-common: Switch lmkd to use minfree levels for kill decisions
    Enable ro.lmk.use_minfree_levels flag to use minfree levels to decide
    when lmkd should kill. This makes lmkd work the same way lowmemorykiller
    kernel driver used to work.
    
    Bug: 77299493
    Bug: 75322373
    Test: lmkd_unit_test
    Change-Id: Ic29d7fd21af1cb6af3a7d2a175d210775a09987e
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -160,7 +160,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.upgrade_pressure=100 \
     ro.lmk.downgrade_pressure=100 \
     ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
