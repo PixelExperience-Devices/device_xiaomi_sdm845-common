48332717267d00b5a7c32e396f7e085fcba9f353
Jorim Jaggi <jjaggi@google.com>
2018-06-19 17:07:02 +0200

    sdm845-common: Refined early SF configuration for WCG
    Test: Use photos, observe no jank
    Bug: 110112323
    Change-Id: Ib331d0b792ee1790963159a17ec24f36b4a1882a
    Merged-In: Ib331d0b792ee1790963159a17ec24f36b4a1882a
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- common-props.mk
+++ common-props.mk
@@ -188,4 +188,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
-    debug.sf.early_phase_offset_ns=5000000
+    debug.sf.early_phase_offset_ns=500000 \
+    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000
