43618f75d6a7c8a44342cb2fd6699d8f369b1a4f
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:46:45 +0200

    sdm845-common: pocketmode: Add local FileUtils
    * We don't really need lineageos sdk for one class
    
    Change-Id: I5e06015c8ea65399bb3643183dd17641ca0d3ad7
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
+++ pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
@@ -24,8 +24,6 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
-import org.lineageos.internal.util.FileUtils;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
