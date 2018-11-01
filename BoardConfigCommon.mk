3eb76539a59e98fda459e88606cc98bcb84078de
argraur <argraur@gmail.com>
2018-11-01 18:10:05 +0000

    arch: cortex-a75 is armv8-2a and armv8-a
    v8.2-A improvements
    Version 8.2-A includes enhanced memory model, 
    half-precision floating point data processing
    and introduces both RAS (reliability availability serviceability) support
    and statistical profiling extension (SPE).
    
    Change-Id: I4b4e8d6dd774dff03fb3849602447d9f9de70565
    Signed-off-by: argraur <argraur@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -10,7 +10,7 @@ COMMON_PATH := device/xiaomi/sdm845-common
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a75
