4566d4051c1494844cf23a2c88e5a1da26c71450
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-01-16 16:47:02 -0200

    sdm845-common: Add GoogleCamera to vendor.camera.aux.packagelist

--- common-props.mk
+++ common-props.mk
@@ -12,7 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=true \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
+    vendor.camera.aux.packagelist=com.android.camera,com.google.android.GoogleCamera
 
 # CNE and DPM
 PRODUCT_PROPERTY_OVERRIDES += \
