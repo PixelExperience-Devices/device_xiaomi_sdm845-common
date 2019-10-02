40ababf91247d342dd5b806c4f0e55aac75b265b
YumeMichi <do4suki@gmail.com>
2019-10-03 03:48:39 +0800

    sdm845-common: Build WiFi stack from OSS
    Change-Id: Ie24487817dc7821e972fe15a534756355c750e26
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -321,7 +321,16 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service \
+    libqsap_sdk \
+    libQWiFiSoftApCfg \
+    libwifi-hal-qcom \
+    libwpa_client \
+    wificond \
+    wifilogd \
+    wpa_cli \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 # WiFi Display
 #PRODUCT_PACKAGES += \
