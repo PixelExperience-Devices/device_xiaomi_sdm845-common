9b4ebdffb1c15a038c13f1436d007ebad0c71556
Ethan Chen <intervigil@gmail.com>
2019-10-26 19:37:49 -0700

    sdm845-common: Enable Pixel atrace HAL
    Change-Id: I34691e5e909b6ca76dc97fd5c45328e3f80b67e1
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -341,6 +341,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm845
 
+# Tracing
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service.pixel
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
