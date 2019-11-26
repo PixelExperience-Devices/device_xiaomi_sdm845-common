9f19644b5b22bb6645cca3964907fe799348ae39
Robert Shih <robertshih@google.com>
2019-11-25 22:25:41 -0800

    sdm845-common: Enable v1.3 drm plugins
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Iacab99903846ef5d63e7535f163190a9bd476f79
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -181,9 +181,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Health
 PRODUCT_PACKAGES += \
