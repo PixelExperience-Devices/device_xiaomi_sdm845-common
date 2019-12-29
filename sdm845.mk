f62b08370c5d5b19bc5fad531a48b8466fb7c392
Henrique Pereira <hlcpereira@pixelexperience.org>
2019-12-29 14:49:51 -0300

    sdm845-common: Split system properties from vendor
    * Move them to /product also. Follows Pixel 3 series and Pixel 4 series devices behavior
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -59,7 +59,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 # Properties
--include $(LOCAL_PATH)/common-props.mk
+-include $(LOCAL_PATH)/vendor_prop.mk
 
 # Haters gonna hate...
 PRODUCT_CHARACTERISTICS := nosdcard
