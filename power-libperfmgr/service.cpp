fa0048b91f614adc7ef20ef426a52cb23ddc6f66
xNombre <kartapolska@gmail.com>
2020-01-04 15:52:51 +0100

    sdm845-common: libperfmgr: Rename pixel to xiaomi
    Change-Id: Ie8c13105d7425d01323f5baa000c084bf5268cea
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- power-libperfmgr/service.cpp
+++ power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.3-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
