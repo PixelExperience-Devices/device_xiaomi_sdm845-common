644f9c3db8b7d050ddad370047c5c1c964ee8873
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2020-01-07 22:59:43 +0200

    sdm845-common: Set loop max part to 16
    * Cuz PSU wants to be like this
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,7 @@ BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048
-BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.configfs=true
+BOARD_KERNEL_CMDLINE += loop.max_part=16 androidboot.configfs=true
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
