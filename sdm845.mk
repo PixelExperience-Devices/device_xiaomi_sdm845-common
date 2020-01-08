0c48f17882e01922a68ddebfad36c62c89fc8262
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-01-08 14:28:02 -0300

    Revert "sdm845-common: Add updatable_apex.mk"
    * Beryllium doesn't support it as it is a FDE device. This must
      be defined in the device specific trees of the targets that have
      FBE as it's a requirement for PSU/Updatable APEX
    
    * This reverts commit 2b191d6766647f106fcb545056d3d20226c431a9.
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -7,7 +7,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
