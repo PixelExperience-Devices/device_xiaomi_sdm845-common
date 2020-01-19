094898fd500e6e51185c2728fd2fc7fb5b3f2610
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-01-19 14:59:43 +0200

    sdm845-common: Remove modem assertion and move to in-built firmware
    * Based-off from https://github.com/PixelExperience-Devices/device_xiaomi_violet/commit/a975c0e7bfa6963ada0c4bf0f7e4be57192129d9
      and adapted for the sdm845-common platform
    
    Co-authored-by: Dyneteve <dyneteve@pixelexperience.org>

--- releasetools.py
+++ releasetools.py
@@ -18,23 +18,35 @@ import re
 
 def FullOTA_InstallEnd(info):
   input_zip = info.input_zip
+  OTA_UpdateFirmware(info)
   OTA_InstallEnd(info, input_zip)
   return
 
 def IncrementalOTA_InstallEnd(info):
   input_zip = info.target_zip
+  OTA_UpdateFirmware(info)
   OTA_InstallEnd(info, input_zip)
   return
 
-def FullOTA_Assertions(info):
-  input_zip = info.input_zip
-  AddBasebandAssertion(info, input_zip)
-  return
-
-def IncrementalOTA_Assertions(info):
-  input_zip = info.target_zip
-  AddBasebandAssertion(info, input_zip)
-  return
+def OTA_UpdateFirmware(info):
+  info.script.AppendExtra('ui_print("Flashing firmware images");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/abl.elf", "/dev/block/bootdevice/by-name/abl_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/abl.elf", "/dev/block/bootdevice/by-name/abl_b");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/cmnlib64.img", "/dev/block/bootdevice/by-name/cmnlib64_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/aop.img", "/dev/block/bootdevice/by-name/aop_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/devcfg.img", "/dev/block/bootdevice/by-name/devcfg_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/qupfw.img", "/dev/block/bootdevice/by-name/qupfw_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/tz.img", "/dev/block/bootdevice/by-name/tz_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/storsec.img", "/dev/block/bootdevice/by-name/storsec_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/keymaster.img", "/dev/block/bootdevice/by-name/keymaster_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/bluetooth.img", "/dev/block/bootdevice/by-name/bluetooth");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/xbl.img", "/dev/block/bootdevice/by-name/xbl_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/modem.img", "/dev/block/bootdevice/by-name/modem");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/xbl_config.img", "/dev/block/bootdevice/by-name/xbl_config_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/dsp.img", "/dev/block/bootdevice/by-name/dsp");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/logo.img", "/dev/block/bootdevice/by-name/logo");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/cmnlib.img", "/dev/block/bootdevice/by-name/cmnlib_a");')
+  info.script.AppendExtra('package_extract_file("install/firmware-update/hyp.img", "/dev/block/bootdevice/by-name/hyp_a");')
 
 def AddImage(info, input_zip, basename, dest):
   path = "IMAGES/" + basename
@@ -48,16 +60,4 @@ def AddImage(info, input_zip, basename, dest):
 
 def OTA_InstallEnd(info, input_zip):
   AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  return
-
-def AddBasebandAssertion(info, input_zip):
-  android_info = input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-baseband\s*=\s*(.+)', android_info)
-  if m:
-    timestamp, firmware_version = m.group(1).rstrip().split(',')
-    timestamps = timestamp.split('|')
-    if ((len(timestamps) and '*' not in timestamps) and \
-        (len(firmware_version) and '*' not in firmware_version)):
-      cmd = 'assert(xiaomi.verify_baseband(' + ','.join(['"%s"' % baseband for baseband in timestamps]) + ') == "1" || abort("ERROR: This package requires firmware from MIUI {1} or newer. Please upgrade firmware and retry!"););'
-      info.script.AppendExtra(cmd.format(timestamps, firmware_version))
   return
