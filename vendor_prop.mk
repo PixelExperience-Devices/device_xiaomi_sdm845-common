43e1cde58130b9e341f4a03c5a01a4bb36fae9eb
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-02-27 12:21:35 -0300

    Revert "sdm845-common: Set lmkd configuration properties"
    This reverts commit e87ff95df7b2a06212d6f64f1f35c286ae8d5e74.
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -151,17 +151,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.audio.soundtrigger.training.level=60 \
     ro.vendor.audio.soundtrigger.hist.duration=1500
 
-# LMKD
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=0 \
-    ro.lmk.critical=0 \
-    ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100
-
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.bservice_enable=true
