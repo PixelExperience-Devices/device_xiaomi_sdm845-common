71b7b8a6c2db03a6be2a02f25379b9b751fbcceb
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-03-06 13:40:32 -0300

    sdm845-common: Build LiveDisplay
    * Let's-a-go
    * This reverts commit f3b58a8744589a0b21e072c7257b373bc22e7792.
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm845.mk
+++ sdm845.mk
@@ -237,6 +237,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm845
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
