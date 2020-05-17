8bcf9b8bbf8c7c2096a92bab961a82f27656f952
Chenyang Zhong <zhongcy95@gmail.com>
2020-05-17 03:49:06 -0400

    sdm845-common: bump vendor.display.config version
    to match the display hal
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -164,8 +164,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libtinyxml \
     libvulkan \
-    vendor.display.config@1.7 \
-    vendor.display.config@1.7.vendor \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9.vendor \
     memtrack.sdm845 \
     vendor.qti.hardware.display.allocator@1.0-service
 
