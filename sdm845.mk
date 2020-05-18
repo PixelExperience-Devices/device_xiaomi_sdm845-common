f7e6cf7e44a0c14f14a87b758a4c9dc478d97886
Pig <pig.priv@gmail.com>
2020-05-18 19:04:57 +0200

    sdm845-common: fingerprint: Add Xiaomi fingerprintextension support
    Co-authored-by: C457 <android.c357@gmail.com>
    Change-Id: Ie9deb0dfcc689034cd3c4b9095e319130ba8d8cd

--- sdm845.mk
+++ sdm845.mk
@@ -318,7 +318,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
