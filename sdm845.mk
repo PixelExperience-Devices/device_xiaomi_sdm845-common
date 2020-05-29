9910b1a0eaba2cc4699fedef062d2bf4ec2ee831
Yan Wang <yawanng@google.com>
2020-05-29 03:00:19 +0000

    sdm845-common: iorap: Turn pinner on for camera
    Bug: 157667363
    Test: Check the pinner in dumpsys
    Change-Id: I6313e4c7a0ad0e8236390d773f29b2109a82c615
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -14,6 +14,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vendor.mk)
 
+include build/make/target/product/iorap_large_memory_config.mk
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
