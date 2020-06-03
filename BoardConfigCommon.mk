7116794bbabb2fce14ae99953a61fc22229b1704
Bruno Martins <bgcngm@gmail.com>
2020-06-03 19:00:32 +0100

    sdm845-common: Update to MIUI V11.0.5.0.QDGMIXM global stable blobs
    Reflect the new source of blobs and matching vendor SPL.
    
    Change-Id: Ic6149e0638e401bd10895abcf1028ced66a54797
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,7 +146,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-03-01
+VENDOR_SECURITY_PATCH := 2020-05-01
 
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
