3b2dba9e2c602f3c2b2acecd81dfa40fab709c7d
Chenyang Zhong <zhongcy95@gmail.com>
2020-07-13 19:59:56 -0400

    Revert "[DNM][TMP]sdm845-common: Switch to 4.0 audio/effects"
    This reverts commit 7cdc00dfe8f55f68f4120da090de8578ce91391f.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -74,9 +74,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.r_submix.default \
