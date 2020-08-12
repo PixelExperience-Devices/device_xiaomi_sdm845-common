046479180fccf0a91fe8e567a39a93a9c56c1963
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sdm845-common: create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -272,6 +272,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# Perfd (dummy)
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.xiaomi-libperfmgr \
