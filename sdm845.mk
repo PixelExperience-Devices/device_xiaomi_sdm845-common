e466384732682c000ae2968b746d1d4ac67c9e4b
Arseniy Graur <argraur@gmail.com>
2020-08-18 10:56:49 +0200

    sdm845-common: Switch to Pixel's thermal 2.0 service
    Use configs from crosshatch
    Also use thermal-engine configs from crosshatch
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -344,11 +344,20 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
+# Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm845
+    android.hardware.thermal@2.0-service.pixel \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/thermal_info_config_qcom.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
+    $(LOCAL_PATH)/thermal_info_config_qcom_evt.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_evt.json
+
+# Thermal-engine
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/thermal-engine-qcom-novr-evt.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-qcom-novr-evt.conf \
+    $(LOCAL_PATH)/thermal-engine-qcom-novr-prod.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-qcom-novr-prod.conf \
+    $(LOCAL_PATH)/thermal-engine-qcom-vr-evt.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-qcom-vr-evt.conf \
+    $(LOCAL_PATH)/thermal-engine-qcom-vr-prod.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-qcom-vr-prod.conf
 
 # Tracing
 PRODUCT_PACKAGES += \
