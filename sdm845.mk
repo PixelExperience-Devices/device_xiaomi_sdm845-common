97521a824f40ae790c5707ea96603abc9cefa54f
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-09-09 21:05:58 -0300

    sdm845-common: Build vendor HWBinder
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -104,6 +104,10 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder.vendor
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
