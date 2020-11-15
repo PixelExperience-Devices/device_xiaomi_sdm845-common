e1dec5a58cec74a38f1a7f4a10967e08aceb7994
Chenyang Zhong <zhongcy95@gmail.com>
2020-11-15 03:46:16 -0500

    sdm845-common: build 64bit vendor variant of libavservices_minijail
    F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/ims_rtp_daemon": library "libavservices_minijail.so" not found: needed by main executable
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -244,6 +244,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libOmxAacEnc \
     libOmxAmrEnc \
