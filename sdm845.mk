1eea58072fe5446c77306050d16e9986d6c74248
Michael Bestas <mkbestas@lineageos.org>
2020-11-23 20:47:24 +0200

    sdm845-common: Don't build textclassifier
    * Target no longer exists
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -340,10 +340,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal HAL
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.pixel \
