9b4478038af9401cfa743882d1acdde1f62d09de
Chenyang Zhong <zhongcy95@gmail.com>
2020-12-23 05:31:04 -0500

    sdm845-common: drop soundtrigger hal
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -73,7 +73,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
