80da3d03e635142c1e22ce6758ab2214de3373b7
Chenyang Zhong <zhongcy95@gmail.com>
2020-12-30 21:49:49 -0500

    sdm845-common: power: implement cached DT2W path
    Iterating through all input nodes to look for the correct one on
    every DT2W event induces higher latency and wastes power. DT2W
    path is fixed for each touchscreen variant, so frequent look-up
    at runtime is unnecessary.
    
    Therefore, cache the DT2W path after the first successful lookup
    and use the cached value for later DT2W events.
    
    Test: boot, examine boot logs, and check double tap to wake
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- power-libperfmgr/aidl/Power.cpp
+++ power-libperfmgr/aidl/Power.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2020 The PixelExperience Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +53,8 @@ Power::Power(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> d
     : mHintManager(hm),
       mDisplayLowPower(dlpw),
       mInteractionHandler(nullptr),
-      mSustainedPerfModeOn(false) {
+      mSustainedPerfModeOn(false),
+      mPathCached(false) {
     mInteractionHandler = std::make_unique<InteractionHandler>(mHintManager);
     mInteractionHandler->Init();
 
@@ -81,7 +83,12 @@ Power::Power(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> d
     ALOGI("PowerHAL ready to process hints");
 }
 
-static int open_ts_input() {
+int Power::open_ts_input() {
+    if (mPathCached) {
+        LOG(DEBUG) << "Using cached DT2W path.";
+        return (open(mDt2wPath, O_RDWR));
+    }
+
     int fd = -1;
     DIR *dir = opendir("/dev/input");
 
@@ -101,8 +108,13 @@ static int open_ts_input() {
                     if (strcmp(name, "atmel_mxt_ts") == 0 || strcmp(name, "fts_ts") == 0 ||
                             strcmp(name, "fts") == 0 || strcmp(name, "ft5x46") == 0 ||
                             strcmp(name, "synaptics_dsx") == 0 ||
-                            strcmp(name, "NVTCapacitiveTouchScreen") == 0)
+                            strcmp(name, "NVTCapacitiveTouchScreen") == 0) {
+                        // cache the dt2w node after finding a match
+                        LOG(INFO) << "Found and cached a valid DT2W node: " << absolute_path;
+                        strncpy(mDt2wPath, absolute_path, PATH_MAX);
+                        mPathCached = true;
                         break;
+                    }
                 }
 
                 close(fd);
@@ -116,7 +128,7 @@ static int open_ts_input() {
     return fd;
 }
 
-static void handle_dt2w(bool enabled) {
+void Power::handle_dt2w(bool enabled) {
     char buf[80];
     int len;
 
@@ -134,6 +146,9 @@ static void handle_dt2w(bool enabled) {
     if (len < 0) {
         strerror_r(errno, buf, sizeof(buf));
         ALOGE("Error writing to fd %d: %s\n", fd, buf);
+        // invalidate the dt2w path cache
+        LOG(INFO) << "Invaliding the DT2W node cache.";
+        mPathCached = false;
     }
     close(fd);
 }
