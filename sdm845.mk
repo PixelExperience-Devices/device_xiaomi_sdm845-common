aab507da4d3db4f77e6e5e110d13ae17d9d1d8a2
Chenyang Zhong <zhongcy95@gmail.com>
2021-04-15 05:34:29 -0400

    sdm845-common: move dummy metadata to init
    system/sepolicy/private/apexd.te has neverallows that prevent
    vendor_init from creating directories labeled as apex_metadata_file.
    Move the code to /system/etc/init/apex_metadata.rc so that scontext
    becomes init instead of vendor_init.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -148,7 +148,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    apex_metadata.rc
 
 # Context Hub
 PRODUCT_PACKAGES += \
