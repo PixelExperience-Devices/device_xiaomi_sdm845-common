d871c45ceceff28ae27d3b8a371fddac309812fa
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-06 01:03:46 -0400

    Revert "sdm845-common: Split component-overrides to support pure AOSP/QTI BT stack"
    This reverts commit 1020ce14c6ce5f8ffa9bd9bc291873f979c072b5.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -18,8 +18,7 @@ include build/make/target/product/iorap_large_memory_config.mk
 
 # Component overrides
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
-    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
