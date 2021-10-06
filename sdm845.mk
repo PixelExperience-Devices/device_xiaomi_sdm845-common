ef38e45024687eb7256179902fcfa0b6ada7c208
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-06 01:03:48 -0400

    Revert "sdm845-common: Drop no longer existing BluetoothQti"
    This reverts commit 316430f4f41ab04b2ffc1f9bced7bcdd6002389a.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -114,6 +114,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl:32 \
     audio.bluetooth.default \
+    BluetoothQti \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
