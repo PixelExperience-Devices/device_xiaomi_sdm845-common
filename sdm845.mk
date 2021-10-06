3268cee82867816163c5d2bbdbd9123b8aeb2790
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-06 01:04:34 -0400

    Revert "Revert "Revert "sdm845-common: Import QCOM bluetooth soong namespace"""
    This reverts commit 0c21ccdb4bde8c81cc1d0817824069d37cb6fe9b.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -327,9 +327,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf
+    hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
