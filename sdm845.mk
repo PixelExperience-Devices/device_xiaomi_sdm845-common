31df24f2701d5a3a20527a586b5bdd557f9c2386
Gabriel Biren <gbiren@google.com>
2023-10-27 22:57:29 +0200

    sdm845-common: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- sdm845.mk
+++ sdm845.mk
@@ -452,7 +452,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libqsap_sdk \
     libQWiFiSoftApCfg \
